e9a0705384af08fcc66b7600fdc8145ea477b8da
dawood <sulaimondawod@gmail.com>
2025-05-30 13:13:04 +0100

    added cors configuration

--- src/main/java/com/dawood/enchry/config/AppConfig.java
+++ src/main/java/com/dawood/enchry/config/AppConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,6 +29,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class AppConfig {
     private final CustomUserDetailsService userDetailsService;
     private final  JwtFilter jwtFilter;
+    private final AuthenticationManager authenticationManager;
 
 
     @Bean
@@ -33,12 +39,26 @@ public class AppConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
 
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource (){
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:3001"));
+        cors.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE"));
+        cors.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource urlSource = new UrlBasedCorsConfigurationSource();
+        urlSource.registerCorsConfiguration("/**",cors);
+
+        return urlSource;
     }
 
     @Bean
