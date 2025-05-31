af72c99cd06f1d36f2caff4d890d77a2932f5383
dawood <sulaimondawod@gmail.com>
2025-05-31 14:01:35 +0100

    setting up device service

--- src/main/java/com/dawood/enchry/service/UserService.java
+++ src/main/java/com/dawood/enchry/service/UserService.java
@@ -4,6 +4,7 @@ import com.dawood.enchry.exception.EmailAlreadyExists;
 import com.dawood.enchry.exception.UserNotFoundException;
 import com.dawood.enchry.model.User;
 import com.dawood.enchry.repository.UserRepository;
+import com.dawood.enchry.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
 
     public boolean existsByEmail(String email){
       return userRepository.existsByEmail(email);
     }
 
     public User findUserByEmail(String email){
-        return  userRepository.findByEmail(email).orElseThrow(()->new UserNotFoundException("Incorrect credentials"));
+        return  userRepository.findByEmail(email).orElseThrow(()->new UserNotFoundException("User credentials not found"));
+    }
+
+    public User extractUserFromToken(String jwt){
+        String userEmail = jwtUtils.extractEmail(jwt);
+        return findUserByEmail(userEmail);
     }
 }
--- src/main/java/com/dawood/enchry/service/device/DeviceService.java
+++ src/main/java/com/dawood/enchry/service/device/DeviceService.java
@@ -1,8 +1,24 @@
 package com.dawood.enchry.service.device;
 
+import com.dawood.enchry.dto.device.DeviceRequestResponseDTO;
+import com.dawood.enchry.model.Device;
+import com.dawood.enchry.repository.DeviceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DeviceService {
+
+    private final DeviceRepository deviceRepository;
+
+    public DeviceRequestResponseDTO addDevice(DeviceRequestResponseDTO req){
+        Device device = new Device();
+
+
+        device.setDeviceName(req.getDeviceName());
+        device.setSensorType(req.getSensorType());
+        device.set
+    }
 
 }
